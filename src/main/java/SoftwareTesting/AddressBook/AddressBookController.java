40290a7ba9c5949ef42a9b4b7a380dfd4873053a
Jason <jsn_ricardo@yahoo.com>
2016-02-04 16:14:04 -0500

    Moved AddressBook to deletion and chaned AddressBook Controller

--- src/main/java/SoftwareTesting/AddressBook/AddressBookController.java
+++ src/main/java/SoftwareTesting/AddressBook/AddressBookController.java
@@ -1,8 +1,28 @@
 package SoftwareTesting.AddressBook;
 
+import java.util.ArrayList;
+
 public class AddressBookController {
+	ArrayList<Person> entries;
 	
-	public void addPerson(){}
+	public AddressBookController(){
+		 entries= new ArrayList<Person>();
+	}
+	
+	public void addEntry(Person person){
+		
+		entries.add(person);
+	}
+	public Person findEntry(String firstName, String lastName){
+		for(int i=0;i<entries.size();i++){
+			if(entries.get(i).getFirstName().matches(firstName)&&entries.get(i).getLastName().matches(lastName)){
+				return entries.get(i);
+			}
+		}
+		
+		return null;
+	}
+
 	public void editPerson(){}
 	public void deletePerson(){}
 	
