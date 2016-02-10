e55ef73a44f311d508cce1cda83e9902f7003fc5
Jason <Jason@Destella.com>
2016-02-09 23:11:18 -0500

    Tester

--- src/main/java/SoftwareTesting/AddressBook/App.java
+++ src/main/java/SoftwareTesting/AddressBook/App.java
@@ -10,5 +10,15 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Testing push from Kyle" );
+        AddressBookController s=new AddressBookController();
+        s.addEntry(new Person("Jason","Ricardo"));
+        s.addEntry(new Person("Adam","Roberts"));
+        s.addEntry(new Person("Adam","Anderson"));
+        s.addEntry(new Person("Lidia","Verse"));
+        s.addEntry(new Person("Ashley","Anderson"));
+        s.addEntry(new Person("Rachel","Rechanek"));
+        s.sortEntries();
+        
+        s.printOut();
     }
 }
