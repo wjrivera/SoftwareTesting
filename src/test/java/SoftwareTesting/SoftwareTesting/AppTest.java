39427bc59eead131e4de7c1f0585fc7c33ee4e3f
Jason <Jason@Destella.com>
2016-02-11 09:22:01 -0500

    Test cases

--- src/test/java/SoftwareTesting/SoftwareTesting/AppTest.java
+++ src/test/java/SoftwareTesting/SoftwareTesting/AppTest.java
@@ -1,5 +1,6 @@
 package SoftwareTesting.SoftwareTesting;
 
+import SoftwareTesting.AddressBook.Person;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
