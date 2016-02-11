d8926556f7446d09d1fb5053a960b65015d48235
kazinsser <kazinsser@gmail.com>
2016-02-11 10:46:47 -0500

    Changed to JUnit4

--- src/SoftwareTesting/SoftwareTesting/AppTest.java
+++ src/SoftwareTesting/SoftwareTesting/AppTest.java
@@ -1,47 +1,39 @@
 package SoftwareTesting.SoftwareTesting;
 
+//import junit.framework.Test;
+//import junit.framework.TestCase;
+//import junit.framework.TestSuite;
+import static org.junit.Assert.*;
 import SoftwareTesting.AddressBook.Person;
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
+	@Before
+	public void setUp() throws Exception {
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
     public void testApp()
     {
         assertTrue( true );
     }
-	
+    @Test
 	public void testPerson(){
     	Person bobPerson = new Person("Bob", "Saget", "1000 Maple Street", "San Francisco", "CA", "55555", "1234567890");
     	assertEquals(bobPerson.getFirstName(), "Bob");
     	assertEquals(bobPerson.getLastName(), "Saget");
    }
+    @Test
     public void testPersonDetails(){
     	Person bobPerson = new Person("Bob", "Saget", "1000 Maple Street", "San Francisco", "CA", "55555", "1234567890");
     	assertEquals(bobPerson.getAddress(), "1000 Maple Street");
